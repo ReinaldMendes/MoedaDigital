0b53310bd77af7156b8ddd1d01f43be91e58e0de
Reinald Mendes <reinald.2967@aluno.pr.senac.br>
2024-10-30 11:49:21 -0300

    ok

---
+++ src/routers/usuario_router.js
@@ -0,0 +1,28 @@
+import { Router } from "express";
+import {
+  signup,
+  login,
+  store,
+  index,
+  update,
+  destroy,
+} from "../controllers/usuario_controller.js";
+import jwtAuthenticator from "../middlewares/jwt-autenticator.js"; // Middleware para autenticação JWT
+import authorizer from "../middlewares/authorizer.js"; // Middleware para autorização por role
+
+const router = Router();
+
+router.post("/signup", signup);
+
+router.post("/login", login);
+
+router.use(jwtAuthenticator);
+router.post("/follow:id", followUnfollow);
+router.delete("/Unfollow/:id", followUnfollow);
+router.use(authorizer("ADMINISTRATOR", "SUPORT"));
+router.get("/", index);
+router.post("/", store); // Criar usuário
+router.put("/:id", update); // Atualizar usuário
+router.delete("/:id", destroy); // Deletar usuário
+
+export default router;
