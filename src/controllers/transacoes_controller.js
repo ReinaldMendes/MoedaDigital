3079cc37324fbf26ed6448c85a1db8ea242eacbe
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-10-31 09:50:44 -0300

    Update transacoes_controller.js

--- src/controllers/transacoes_controller.js
+++ src/controllers/transacoes_controller.js
@@ -3,45 +3,49 @@ import transacao from "../models/transacoes_model.js";
 export const store = async (req, resp) => {
   try {
     const content = await transacao.create(req.body);
-    resp.json();
+    resp.status(201).json(content);  // Retorna o conteúdo criado e status 201
   } catch (error) {
-    resp.json(error);
+    resp.status(400).json(error);
   }
 };
+
 export const index = async (req, resp) => {
   try {
     const content = await transacao.find().exec();
     resp.json(content);
   } catch (error) {
-    resp.json(error);
+    resp.status(400).json(error);
   }
 };
+
 export const show = async (req, resp) => {
   try {
     const content = await transacao
       .findById(req.params.id)
-      .populate("userId")
+      .populate(["walletFrom", "walletTo"])  // Popula as carteiras associadas
       .exec();
     resp.json(content);
   } catch (error) {
-    resp.json(error);
+    resp.status(400).json(error);
   }
 };
+
 export const update = async (req, res) => {
   try {
     const content = await transacao
-      .findByIdAndUpdate(req.params.id, req.body)
+      .findByIdAndUpdate(req.params.id, req.body, { new: true })  // Retorna a transação atualizada
       .exec();
     res.json(content);
   } catch (error) {
     res.status(400).send(error.message);
   }
 };
+
 export const destroy = async (req, resp) => {
   try {
-    transacao.findByIdAndDelete(req.params.id).exec();
-    resp.json();
+    await transacao.findByIdAndDelete(req.params.id).exec();
+    resp.status(204).json();  // Retorna status 204 sem conteúdo
   } catch (error) {
-    resp.json(error);
+    resp.status(400).json(error);
   }
 };
