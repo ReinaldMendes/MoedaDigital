ad5e338a2a9c418fd94bef1a7da9223f7a97f0c9
Reinald Mendes <112445979+ReinaldMendes@users.noreply.github.com>
2024-11-08 21:43:48 -0300

    Update carteira_router.js

--- src/routers/carteira_router.js
+++ src/routers/carteira_router.js
@@ -11,16 +11,17 @@ import jwtAuthenticator from "../middlewares/jwt-autenticator.js"; // Middleware
 import authorizer from "../middlewares/authorizer.js"; // Middleware para autorização por role
 
 const router = Router();
-router.get("/balance/:userId", getBalance);
+
 // Middleware para autenticação e autorização
 router.use(jwtAuthenticator);
 router.use(authorizer("ADMINISTRATOR", "SUPPORT"));
 
+// Rota para obter o saldo de um usuário autenticado
+router.get("/balance/:userId", getBalance); // agora protegida pelo jwtAuthenticator e authorizer
+
 // Rotas da carteira
 router.get("/", index); // Listar todas as carteiras
 router.get("/:id", show); // Exibir uma carteira específica pelo ID
-
-
 router.post("/", store); // Criar carteira
 router.put("/:id", update); // Atualizar carteira
 router.delete("/:id", destroy); // Deletar carteira
